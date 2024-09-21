9df304f8561857744c64d299f338fe3fe9d77f09
Gokhan Taskan <gokhan.frontfeed@gmail.com>
2024-09-21 21:34:29 +0300

    refactor: remove tailwindcss dependency from the ui styles

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -2,9 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ["@acme/ui/main.scss"],
+  css: ["@acme/ui/main.scss", "~/assets/styles/main.scss"],
   future: { compatibilityVersion: 4 },
   typescript: { typeCheck: true },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap",
+        },
+      ],
+    },
+  },
   modules: [
     [
       "@nuxtjs/tailwindcss",
--- apps/client/pages/index.vue
+++ apps/client/pages/index.vue
@@ -5,6 +5,11 @@ import { Button } from "@acme/ui";
 <template>
   <div class="container">
     <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam explicabo autem eos?
+      Reprehenderit dolor incidunt ullam ducimus earum recusandae nisi modi deserunt enim!
+      Doloremque hic voluptatibus dignissimos voluptatem quisquam cupiditate.
+    </p>
     <div class="space-x-2">
       <Button
         variant="primary"
